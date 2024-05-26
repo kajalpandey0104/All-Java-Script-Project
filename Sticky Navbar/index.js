2a48328406c5f273945a2ff1c1e47b1fecc3f47a
Kajal Pandey <kajalpandeymzp@gmail.com>
2024-05-27 00:27:22 +0530

    updated code

--- Sticky Navbar/index.js
+++ Sticky Navbar/index.js
@@ -1,8 +1,18 @@
-const navbarE1=document.querySelector(".navbar");
+const navbarEl = document.querySelector(".navbar");
 
+const bottomContainerEl = document.querySelector(".bottom-container");
 
+console.log(navbarEl.offsetHeight);
 
+console.log(bottomContainerEl.offsetTop);
 
-
-
-console.log(navbarE1);
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY >
+    bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50
+  ) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
+});
